ef8a9b2eb4e762293303c9bb7d137b779cb3c91d
liwenzhong <1501391805@qq.com>
2019-08-25 22:02:16 +0800

    首页布局初步完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,21 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '@/views/login'
+import Home from '@/views/home'
+import Welcome from '@/views/welcome'
+import NotFound from '@/views/404'
 Vue.use(VueRouter)
 var router = new VueRouter({
   routes: [
-    { path: '/lg', name: 'login', component: Login }
+    { path: '/login', name: 'login', component: Login },
+    { path: '/', component: Home },
+    { path: '/home',
+      component: Home,
+      children: [
+        { path: '/', name: 'Welcome', component: Welcome }
+      ]
+    },
+    { path: '*', name: 404, component: NotFound }
   ]
 })
 export default router
